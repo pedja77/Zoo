6fe3e511a23d04a4e10f6a0d731849ba6db06da9
pedja77 <pedja77@stcable.net>
2018-04-24 18:30:32 +0200

    Animals list shown

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AnimalsList></AnimalsList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnimalsList from './components/AnimalsList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AnimalsList
   }
 }
 </script>
---
+++ src/components/AnimalsList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="animals">
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Species</th>
+        <th>Born on</th>
+      </thead>
+      <tr v-for="(animal, key) in animals" :key="key">
+        <td>{{ animal.name  }}</td>
+        <td>{{ animal.species  }}</td>
+        <td>{{ animal.dateOfBirth  }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalsList',
+  props: {
+    
+  },
+  data() {
+    return {
+      animals: [
+        { name: 'Polly', species: 'Parrot', dateOfBirth: '15/02/1899' },
+        { name: 'Molly', species: 'Antilope', dateOfBirth: '15/02/1959' },
+        { name: 'Dumbo', species: 'Elephant', dateOfBirth: '15/02/1989' },
+        { name: 'Peter', species: 'Rabbit', dateOfBirth: '15/02/1969' },
+        { name: 'Bagira', species: 'Panther', dateOfBirth: '16/03/1999' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
